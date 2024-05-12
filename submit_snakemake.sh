7e771e8dc4d912312f25898b886face9fd1a4f1c
Rhayania <dragaragazza@gmail.com>
2024-05-12 00:25:27 -0600

    increased CPU # request to support trinity assembly

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=logs/snakemake_err.%j.err      # Error file
 #SBATCH --nodes=1                              # Number of nodes
 #SBATCH --ntasks-per-node=1                    # Number of tasks per node
-#SBATCH --cpus-per-task=4                      # Number of CPU cores per task
+#SBATCH --cpus-per-task=12                      # Number of CPU cores per task
 #SBATCH --time=6:00:00                         # Time limit (format: HH:MM:SS)
 
 # Load modules
